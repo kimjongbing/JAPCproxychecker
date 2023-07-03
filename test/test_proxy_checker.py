900d786790544081344fb0b7b29fd8ef49d72fdf
kimjongbing <126769537+kimjongbing@users.noreply.github.com>
2023-07-03 14:03:29 +1000

    updated to add fail tests. probably works fine. can't really tell since the debugger messes up

--- test/test_proxy_checker.py
+++ test/test_proxy_checker.py
@@ -2,7 +2,6 @@ import unittest
 from unittest.mock import patch, Mock
 from src.proxy_checker import ProxyChecker
 
-
 class TestProxyChecker(unittest.TestCase):
     def setUp(self):
         self.proxies = ["proxy1", "proxy2", "proxy3"]
@@ -10,13 +9,21 @@ class TestProxyChecker(unittest.TestCase):
 
     @patch("src.proxy_checker.requests.get")
     def test_check_proxy(self, mock_get):
-        mock_response = Mock()
-        mock_response.status_code = 200
-        mock_get.return_value = mock_response
+        mock_response_200 = Mock()
+        mock_response_200.status_code = 200
+        mock_response_404 = Mock()
+        mock_response_404.status_code = 404
+        mock_response_500 = Mock()
+        mock_response_500.status_code = 500
+        mock_get.side_effect = [mock_response_200, mock_response_404, mock_response_500]
 
-        for proxy in self.proxies:
+        expected_results = {200: "proxy1", 404: "proxy2", 500: "proxy3"}
+        for status_code, proxy in expected_results.items():
             result = self.checker.check_proxy(proxy)
-            self.assertEqual(result, proxy)
+            if status_code == 200:
+                self.assertEqual(result, proxy)
+            else:
+                self.assertIsNone(result)
             mock_get.assert_called_with(
                 "http://google.com", proxies={"http": proxy, "https": proxy}, timeout=5
             )
