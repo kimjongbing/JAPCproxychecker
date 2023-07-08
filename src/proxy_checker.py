8e53c320f518dac1e1463a48f6eb600d919a3fdb
kimjongbing <126769537+kimjongbing@users.noreply.github.com>
2023-07-09 00:25:38 +1000

    increased readability of proxy_checker, also more: - removed default config directory from config.json (for now).

--- src/proxy_checker.py
+++ src/proxy_checker.py
@@ -11,46 +11,52 @@ class ProxyChecker:
         self.total_proxies = len(proxies)
         self.counter = Counter()
 
-    def check_proxy(self, proxy):
-        print(f"Checking proxy: {proxy}")
-        parsed_proxy = urlparse(proxy)
+    def get_schemes(self, parsed_proxy):
         if parsed_proxy.scheme:
-            schemes = [parsed_proxy.scheme + "://"]
+            return [parsed_proxy.scheme + "://"]
         elif self.scheme:
-            schemes = [self.scheme + "://"]
+            return [self.scheme + "://"]
         else:
-            schemes = ["http://", "https://", "socks5://", "socks4://"]
+            return ["http://", "https://", "socks5://", "socks4://"]
 
+    def format_proxy(self, scheme, proxy, parsed_proxy):
+        if parsed_proxy.scheme:
+            return proxy
+        else:
+            return scheme + proxy
+
+    def get_response(self, formatted_proxy):
+        proxies = {"http": formatted_proxy, "https": formatted_proxy}
+        try:
+            return requests.get("http://google.com", proxies=proxies, timeout=5)
+        except (
+            requests.exceptions.RequestException,
+            requests.exceptions.ConnectTimeout,
+            requests.exceptions.ReadTimeout,
+        ):
+            return None
+
+    def check_proxy(self, proxy):
+        parsed_proxy = urlparse(proxy)
+        schemes = self.get_schemes(parsed_proxy)
         status = None
+
         for scheme in schemes:
-            try:
-                if parsed_proxy.scheme:
-                    formatted_proxy = proxy
-                else:
-                    formatted_proxy = scheme + proxy
-                print(f"Scheme: {scheme}, Formatted proxy: {formatted_proxy}")
+            formatted_proxy = self.format_proxy(scheme, proxy, parsed_proxy)
+            response = self.get_response(formatted_proxy)
 
-                proxies = {"http": formatted_proxy, "https": formatted_proxy}
-                response = requests.get("http://google.com", proxies=proxies, timeout=5)
-                status = response.status_code
-                if status == 200:
-                    print(f"Proxy {proxy} is good with {scheme[:-3]} scheme.")
-                else:
-                    print(
-                        f"Proxy {proxy} failed with status code {status} for {scheme[-3]} scheme."
-                    )
-            except (
-                requests.exceptions.RequestException,
-                requests.exceptions.ConnectTimeout,
-                requests.exceptions.ReadTimeout,
-            ):
-                print(f"Proxy {proxy} failed for {scheme[:-3]} scheme.")
-                continue
+            if response is not None and response.status_code == 200:
+                print(f"Proxy {proxy} is good with {scheme[:-3]} scheme.")
+                status = 200
+            else:
+                print(
+                    f"Proxy {proxy} failed with status code {status} for {scheme} scheme."
+                )
 
-        self.counter.increment()
-        print(f"Checked {self.counter.value()} out of {self.total_proxies}")
+            self.counter.increment()
+            print((f"Checked {self.counter.value()} out of {self.total_proxies}"))
 
-        return proxy if status == 200 else None
+            return proxy if status == 200 else None
 
     def filter_proxies(self):
         with concurrent.futures.ThreadPoolExecutor(max_workers=1000) as executor:
