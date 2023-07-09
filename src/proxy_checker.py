7bd053cf80090f8f80607927aa2c6ef904855cd1
kimjongbing <126769537+kimjongbing@users.noreply.github.com>
2023-07-09 19:53:12 +1000

    output cleaned up, nesting hell introduced. needs serious refactoring

--- src/proxy_checker.py
+++ src/proxy_checker.py
@@ -1,7 +1,13 @@
 import requests
 import concurrent.futures
+import logging
 from urllib.parse import urlparse
 from .counter import Counter
+from colorama import Fore, Style
+from tqdm import tqdm
+
+error_logger = logging.getLogger("error_logger")
+error_logger.setLevel(logging.ERROR)
 
 
 class ProxyChecker:
@@ -33,6 +39,7 @@ class ProxyChecker:
             requests.exceptions.RequestException,
             requests.exceptions.ConnectTimeout,
             requests.exceptions.ReadTimeout,
+            requests.exceptions.SSLError,
         ):
             return None
 
@@ -46,18 +53,52 @@ class ProxyChecker:
             response = self.get_response(formatted_proxy)
 
             if response is not None and response.status_code == 200:
-                print(f"Proxy {proxy} is good with {scheme[:-3]} scheme.")
                 status = 200
-            else:
-                print(
-                    f"Proxy {proxy} failed with status code {status} for {scheme} scheme."
-                )
 
-            self.counter.increment()
-            print((f"Checked {self.counter.value()} out of {self.total_proxies}"))
+        if status == 200:
+            self.counter["Succeeded"] += 1
+        else:
+            self.counter["Failed"] += 1
 
-            return proxy if status == 200 else None
+        return proxy if status == 200 else None
 
     def filter_proxies(self):
+        requests.packages.urllib3.disable_warnings()
+        working_proxies = []
+        self.counter = {"Checked": 0, "Succeeded": 0, "Failed": 0}
         with concurrent.futures.ThreadPoolExecutor(max_workers=1000) as executor:
-            return list(filter(None, executor.map(self.check_proxy, self.proxies)))
+            future_to_proxy = {
+                executor.submit(self.check_proxy, proxy): proxy
+                for proxy in self.proxies
+            }
+
+            with tqdm(
+                total=self.total_proxies, bar_format="{l_bar}{bar}| {postfix}"
+            ) as pbar:
+                pbar.set_postfix(self.counter, refresh=True)
+                for future in concurrent.futures.as_completed(future_to_proxy):
+                    proxy = future_to_proxy[future]
+                    try:
+                        data = future.result()
+                    except Exception as exc:
+                        if not error_logger.hasHandlers():
+                            error_file_handler = logging.FileHandler("error.log")
+                            error_formatter = logging.Formatter(
+                                "%(asctime)s - %(levelname)s - %(message)s"
+                            )
+                            error_file_handler.setFormatter(error_formatter)
+                            error_logger.addHandler(error_file_handler)
+                        error_logger.error(
+                            "%r generated an exception: %s" % (proxy, exc)
+                        )
+                        self.counter["Failed"] += 1
+                    else:
+                        if data is not None:
+                            working_proxies.append(data)
+                            self.counter["Succeeded"] += 1
+                        else:
+                            self.counter["Failed"] += 1
+                    self.counter["Checked"] += 1
+                    pbar.set_postfix(self.counter, refresh=True)
+                    pbar.update(1)
+        return working_proxies
